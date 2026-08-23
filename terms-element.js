323a399991e0b7adc3784907e7b7bc1ff108624e
CodeSF04 <fjuan2356@gmail.com>
2026-08-23 19:45:54 -0300

    Add favicon update method to MergeTerms class

--- terms-element.js
+++ terms-element.js
@@ -34,6 +34,20 @@ const SOUND_BLOCKED_MESSAGES = {
 };
 
 class MergeTerms extends HTMLElement {
+
+  actualizarFavicon() {
+    try {
+      let link = document.querySelector("link[rel*='icon']");
+      if (!link) {
+        link = document.createElement("link");
+        link.rel = "icon";
+        document.head.appendChild(link);
+      }
+      link.type = "image/svg+xml";
+      link.href = "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23eff6ff' stroke='%232a5cff' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z'/><polyline points='14 2 14 8 20 8' fill='%23dbeafe'/><line x1='16' y1='13' x2='8' y2='13'/><line x1='16' y1='17' x2='8' y2='17'/><polyline points='10 9 9 9 8 9'/></svg>";
+    } catch(e) {}
+  }
+
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
@@ -61,6 +75,7 @@ class MergeTerms extends HTMLElement {
   }
 
   connectedCallback() {
+    this.actualizarFavicon();
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme === 'dark') document.documentElement.classList.add('dark-mode');
 
